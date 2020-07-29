89ddef166a8995490a92d8b9136621827352d43f
Hugo Rocha <hugo.rocha@biogerm.pt>
2020-07-29 19:47:23 +0100

    2

---
+++ UTIL Opentrons APP/import opentrons.execute 2 copy.py
@@ -0,0 +1,17 @@
+from opentrons import protocol_api
+
+metadata = {'apiLevel': '2.5'}
+
+NUM_SAMPLES = 32
+SAMPLE_VOLUME = 100
+
+
+def run(protocol: protocol_api.ProtocolContext):
+    sou = protocol.load_labware('opentrons_15_tuberack_falcon_15ml_conical', 4)
+    dest = protocol.load_labware('nest_96_wellplate_200ul_flat', 1)
+    tiprack_1 = protocol.load_labware('opentrons_96_filtertiprack_200ul', 5)
+    p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
+    dests_single = dest.wells()[:NUM_SAMPLES]
+
+    p300.transfer(100, sou.wells_by_name () ['A1'], dests_single )
+
--- UTIL Opentrons APP/import opentrons.execute 2.py
+++ UTIL Opentrons APP/import opentrons.execute 2.py
@@ -23,11 +23,11 @@ def run(ctx: protocol_api.ProtocolContext):
 
     # load labware
 
-    source_racks = [
+    s_racks = [
         ctx.load_labware(
             'opentrons_15_tuberack_falcon_15ml_conical', '4')
     ]
-    dest_plate = ctx.load_labware(
+    d_plate = ctx.load_labware(
         'nest_96_wellplate_200ul_flat', '1', '96-wellplate sample plate')
   
     tips300 = [ctx.load_labware('opentrons_96_filtertiprack_200ul', '5')]
@@ -39,55 +39,15 @@ def run(ctx: protocol_api.ProtocolContext):
     
     
 
-    # setup samples
-    sources = source_racks.wells ('A1')
-    dests_single = dest_plate.wells()[:NUM_SAMPLES]
-
-    tip_log = {'count': {}}
-    folder_path = '/data/A'
-    tip_file_path = folder_path + '/tip_log.json'
-    if TIP_TRACK and not ctx.is_simulating():
-        if os.path.isfile(tip_file_path):
-            with open(tip_file_path) as json_file:
-                data = json.load(json_file)
-                if 'tips300' in data:
-                    tip_log['count'][p300] = data['tips300']
-                else:
-                    tip_log['count'][p300] = 0
-
-    else:
-        tip_log['count'] = {p300: 0}#, m20: 0}
-
-    tip_log['tips'] = {
-        p300: [tip for rack in tips300 for tip in rack.wells()]
-    }
-    tip_log['max'] = {
-        pip: len(tip_log['tips'][pip])
-        for pip in [p300]
-    }
-
-    def pick_up(pip):
-        nonlocal tip_log
-        if tip_log['count'][pip] == tip_log['max'][pip]:
-            ctx.pause('Replace ' + str(pip.max_volume) + 'µl tipracks before \
-resuming.')
-            pip.reset_tipracks()
-            tip_log['count'][pip] = 0
-        pip.pick_up_tip(tip_log['tips'][pip][tip_log['count'][pip]])
-        tip_log['count'][pip] += 1
-
-    # transfer two controls
-    # **Incluir o labware (suporte de eppendorf)
-    # definir local branco
-    # if TRUE reta de calibração
+   
 
     # transfer sample
-    for s, d in zip(sources, dests_single):
-        pick_up(p300)
-        p300.transfer(SAMPLE_VOLUME, s.bottom(5), d.bottom(5), air_gap=20,
+    
+
+    p300.transfer(SAMPLE_VOLUME, s_racks.wells () ['A1'], d_plate.columns()['5'], air_gap=20,
                       mix_before=(2, 200), new_tip='never')
-        p300.air_gap(20)
-        p300.drop_tip()
+    p300.air_gap(20)
+    p300.drop_tip()
 
     
 
