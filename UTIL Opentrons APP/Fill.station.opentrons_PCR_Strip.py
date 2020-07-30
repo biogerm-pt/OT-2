097b131a1b955517793f1796058b08a2b667ead7
MolLab2 <mollab2@MacBook-Air-de-MolLab2.local>
2020-07-30 14:09:51 +0100

    1

--- UTIL Opentrons APP/Fill.station.opentrons_PCR_Strip.py
+++ UTIL Opentrons APP/Fill.station.opentrons_PCR_Strip.py
@@ -2,13 +2,13 @@ from opentrons import protocol_api
 
 metadata = {'apiLevel': '2.5'}
 
-NUM_SAMPLES = 32
-SAMPLE_VOLUME = 100
+NUM_SAMPLES = 8
+SAMPLE_VOLUME = 200
 
 
 def run(protocol: protocol_api.ProtocolContext):
     sou = protocol.load_labware('opentrons_15_tuberack_falcon_15ml_conical', 4)
-    dest = protocol.load_labware('nest_96_wellplate_200ul_flat', 1)
+    dest = protocol.load_labware('opentrons_96_aluminumblock_nest_wellplate_100ul', 1)
     tiprack_1 = protocol.load_labware('opentrons_96_filtertiprack_200ul', 5)
     p300 = protocol.load_instrument('p300_single', 'left', tip_racks=[tiprack_1])
     dests_single = dest.wells()[:NUM_SAMPLES]
