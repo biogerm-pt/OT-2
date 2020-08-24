f67891c2d10d4c245dfcdc3df1c9d3f41a16482c
Ricardo Magalhaes <ricmag@MBPRM.local>
2020-08-24 21:53:33 +0100

    1

--- SARS CoV-2/Station A/MagMax/TEST_v5_station_a_S14_magmax_Multi.py
+++ SARS CoV-2/Station A/MagMax/TEST_v5_station_a_S14_magmax_Multi.py
@@ -151,9 +151,9 @@ resuming.')
                 m300.mix(MIX_REPETITIONS, MIX_VOLUME, source)
             #m300.transfer(vol_per_trans, source, m, air_gap=8, new_tip='never')
             m300.aspirate(vol_per_trans, source)
-            m300.air_gap(5)
+            m300.air_gap(15)
             ctx.delay(seconds=2, msg='eliminar escorrido.')
-            m300.touch_tip()
+            m300.dispense(5, source )
             m300.default_speed = 100
             m300.dispense(vol_per_trans, m )
             m300.default_speed = 400
