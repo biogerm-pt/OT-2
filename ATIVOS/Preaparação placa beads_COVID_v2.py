77bbddf4c8bf32a4e48fc8eeece75b34d80351e8
Ricardo Magalhaes <ricmag@MBPRM.local>
2020-08-25 16:26:33 +0100

    1

--- ATIVOS/Preaparação placa beads_COVID_v2.py
+++ ATIVOS/Preaparação placa beads_COVID_v2.py
@@ -11,7 +11,7 @@ metadata = {
     'apiLevel': '2.0'
 }
 
-NUM_SAMPLES = 28
+NUM_SAMPLES = 16
 TUBE50_VOlUME = 20
 
 BB_VOLUME = 427.5
@@ -149,7 +149,19 @@ resuming.')
         for i in range(num_trans):
             if i == 0:
                 m300.mix(MIX_REPETITIONS, MIX_VOLUME, source)
-            m300.transfer(vol_per_trans, source, m, air_gap=10, new_tip='never')
+            #m300.transfer(vol_per_trans, source, m, air_gap=8, new_tip='never')
+            m300.blow_out()
+            m300.aspirate(vol_per_trans, source)
+            m300.air_gap(12)
+            ctx.delay(seconds=3)
+            m300.dispense(10, source.top() )
+            ctx.delay(seconds=2)
+            m300.default_speed = 100
+            m300.dispense(vol_per_trans, m )
+            m300.blow_out()
+            m300.default_speed = 400
+        
+
     m300.drop_tip()
 
     ctx.comment('Move deepwell plate (slot 4) to Station B for RNA \
